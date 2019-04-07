0db96bd3c46582dcddc8a8fe4edccf2b9bd0cf26
aleks <aleks.valushko@yandex.ru>
2019-04-07 15:05:30 +0300

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", function () {
    'use strict';
 
+// Taby
+
    let tab = document.querySelectorAll('.info-header-tab'),
        info = document.querySelector('.info-header'),
        tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,4 +36,56 @@ window.addEventListener("DOMContentLoaded", function () {
            } 
        }
    });
+
+   //Timer
+
+    let deadline = '2019-04-8';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*60*60));
+
+        return{
+          'total' : t,
+          'hours' : hours,
+          'minutes' : minutes,
+          'seconds' : seconds
+        };
+
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(clockUpdate, 1000);
+
+            function clockUpdate() {
+                let t = getTimeRemaining(endtime);
+                hours.textContent = (t.hours < 10 ? '0' : '') + t.hours;
+                minutes.textContent = (t.minutes < 10 ? '0' : '') + t.minutes;
+                seconds.textContent = (t.seconds < 10 ? '0'  : '') + t.seconds;
+
+                if (t.total <= 0){
+                    clearInterval(timeInterval);
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                }
+            }
+            /*if (t.seconds.length < 2) {  //добавляем 0 t.hours.length < 2 || t.minutes.length < 2 ||
+                t.seconds.push('0');
+            }*/
+
+    }
+
+    setClock('timer', deadline);
+
+    function endDeadline() {
+        
+    }
+
 });
